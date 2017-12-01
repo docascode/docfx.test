7a26a7cc96f9afecb3dbbdc0c5b8b57cc59ebce5
lianwei <lianwei@microsoft.com>
2017-12-01 16:53:00 +0800

    add expected result

---
+++ build.ps1
@@ -0,0 +1,14 @@
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path
+$expectedPath = Join-Path $scriptPath "expectedOutput"
+$actualPath = Join-Path $scriptPath "obj" | Join-Path -ChildPath "api"
+
+$expected = Get-ChildItem -Path "$expectedPath" | foreach {Get-FileHash -Path $_.FullName}
+$actual = Get-ChildItem -Path "$actualPath" | foreach {Get-FileHash -Path $_.FullName}
+
+$diff = Compare-Object -ReferenceObject $expected -DifferenceObject $actual -PassThru -Property Hash | select -expand Path;
+
+$message = $diff -join " != "
+Write-Host $message
+if ($diff.Length -gt 0) {
+    throw "Result different from expected: $message"
+}
