90a29302b40ce914ba112f5785b8bfe394c2e3f5
lianwei <lianwei@microsoft.com>
2017-12-06 09:37:33 +0800

    order

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@ $scriptPath = Split-Path $MyInvocation.MyCommand.Path
 $expectedPath = Join-Path $scriptPath "expectedOutput"
 $actualPath = Join-Path $scriptPath "obj" | Join-Path -ChildPath "api"
 
-$expected = Get-ChildItem -Path "$expectedPath" | foreach {Get-FileHash -Path $_.FullName}
-$actual = Get-ChildItem -Path "$actualPath" | foreach {Get-FileHash -Path $_.FullName}
+$expected = Get-ChildItem -Path "$expectedPath" | sort-object {$_.FullName} | foreach {Get-FileHash -Path $_.FullName}
+$actual = Get-ChildItem -Path "$actualPath" | sort-object {$_.FullName} | foreach {Get-FileHash -Path $_.FullName}
 
 $diff = Compare-Object -ReferenceObject $expected -DifferenceObject $actual -PassThru -Property Hash | select -expand Path;
 
