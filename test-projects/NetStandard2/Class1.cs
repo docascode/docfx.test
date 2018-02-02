eebab1197e658b8fa01cf05fec334f1fe000ec5b
lianwei <lianwei@microsoft.com>
2018-02-02 13:38:55 +0800

    Add net2.0 core app and c# 7.2 features

---
+++ test-projects/NetStandard2/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NetStandard2
+{
+    /// <summary>
+    /// C# 7.2 features
+    /// </summary>
+    /// <seealso cref="Enum"/>
+    public class Class1
+    {
+        private static Class1 origin = new Class1();
+
+        public static ref readonly Class1 Origin => ref origin;
+
+        public Class1()
+        {
+
+        }
+        public Class1(int i, in Class1 transform)
+        {
+
+        }
+
+        private protected void ShouldNotBeInDocs()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Enum
+    /// </summary>
+    /// <seealso cref="Class1"/>
+    public enum Enum
+    { }
+}
